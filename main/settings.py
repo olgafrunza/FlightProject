950e6fd73639a0757ad889f84ed1ff0fc1f080f6
Olga <frunzaov@gmail.com>
2023-05-22 21:22:48 -0400

    postgres settings

--- main/settings.py
+++ main/settings.py
@@ -80,13 +80,23 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": config('SQL_DATABASE'),
+        "USER": config('SQL_USER'),
+        "PASSWORD": config('SQL_PASSWORD'),
+        "HOST" : config('SQL_HOST'),
+        "PORT": config('SQL_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
